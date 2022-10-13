21b32024ff644df5d93c10cf81f61ce15c23ea15
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-10-13 14:55:11 +0530

    Complete Borrower and Lender Functioning

--- contracts/LendingPlatformWithToken.sol
+++ contracts/LendingPlatformWithToken.sol
@@ -6,10 +6,13 @@ import "./LPToken.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-contract LendingPlatformInheritance is LPToken {
+contract LendingPlatformWithToken is LPToken {
     using PriceConverter for uint256;
 
     // address payable public owner;
+
+    // // // // // // // //
+    // Variables for Borrower
 
     // Total Number of Borrowers
     uint256 public totalBorrowers;
@@ -31,6 +34,32 @@ contract LendingPlatformInheritance is LPToken {
     mapping(address => LoanInfo) public BorrowedLoanInfo;
     // address to Interest
     mapping(address => uint256) public interestToBePaid;
+    // // // // // // // //
+
+    // // // // // // // //
+    // Variables for Lender
+
+    // Total Number of Investers
+    uint256 public totalInvester;
+    // Address of all Investers
+    address[] public investers;
+    // Address to InvestmentAmount Mapping
+    mapping(address => uint256) public addressToInvestmentAmountInWei;
+    // Struct of Invester
+    struct InvesterInfo {
+        address investedBy;
+        uint256 investmentAmountInWei;
+        uint256 startInvestmentTime;
+        uint256 amountOfLPGenerated;
+        uint256 ApproxInterestGained;
+        bool didLPReturnedWithProfit;
+    }
+    // Address to Investment Details
+    mapping(address => InvesterInfo) public InvestmentInfo;
+    // address to Interest to be returned
+    mapping(address => uint256) public interestToBeReturnedInWei;
+
+    // // // // // // // //
 
     LPToken public LPTokenContract;
 
@@ -39,17 +68,6 @@ contract LendingPlatformInheritance is LPToken {
         totalBorrowers = 0;
         owner = payable(msg.sender);
     }
-
-    // receive() external payable{
-    // }
-
-    // modifier onlyOwner() {
-    //     require(
-    //         msg.sender == owner,
-    //         "You do not have permission for withdraw!"
-    //     );
-    //     _;
-    // }
 
     // Lets first build Functionality for Borrower
     // He will pay crypto to get stable coin (Here LP)
@@ -95,15 +113,14 @@ contract LendingPlatformInheritance is LPToken {
         uint256 time1;
         uint256 time2;
         uint256 timeDifference;
-        // uint256 time = BorrowedLoanInfo[msg.sender].startLoanTime/6000 - block.timestamp/6000;
-        // uint256 time = BorrowedLoanInfo[msg.sender].startLoanTime + 1 minutes ;
+
         time1 = BorrowedLoanInfo[msg.sender].startLoanTime;
         time2 = block.timestamp;
         timeDifference = time2 - time1;
 
         // Simple Interest
-        // 14% interest for an Year
-        // Therefore (r(in %) * t(in min) = 14*1/100*365*24*60 = 0.000000266362253
+        // 25% interest for an Year
+        // Therefore (r(in %) * t(in min) = 25*1/100*365*24*60 = 0.00000047564688
 
         // Interest based on minutes
         // * Currently I'm Multiplying this equation with 1000 to check interest amount
@@ -112,11 +129,8 @@ contract LendingPlatformInheritance is LPToken {
             (1000 *
                 ((timeDifference / 60) *
                     BorrowedLoanInfo[msg.sender].loanAmountInLP *
-                    14)) /
+                    25)) /
             (100 * 365 * 24 * 60);
-
-        // interestToBePaid[msg.sender] = 100 minutes  * BorrowedLoanInfo[msg.sender].loanAmountInLP * 10 / 10000000;
-        // interestToBePaid[msg.sender] = (100 minutes  * BorrowedLoanInfo[msg.sender].loanAmountInLP * 14) / (100*365*24*60);
     }
 
     // Now Let Him Repay in form of Stable Coins (Here LPs) with interest to get back his Crypto
@@ -130,7 +144,7 @@ contract LendingPlatformInheritance is LPToken {
                         (((block.timestamp -
                             BorrowedLoanInfo[msg.sender].startLoanTime) / 60) *
                             BorrowedLoanInfo[msg.sender].loanAmountInLP *
-                            14)) / (100 * 365 * 24 * 60))
+                            25)) / (100 * 365 * 24 * 60))
         );
 
         // Borrower paying his Loan Amount in LP
@@ -150,15 +164,12 @@ contract LendingPlatformInheritance is LPToken {
         );
         require(success, "Call Failed");
 
-        // // // // CHANGE EVERYTHING BACK TO AS IT WAS
+        // // // // // // // // // // // //
+        // CHANGE EVERYTHING BACK TO AS IT WAS
 
-        // !!!!!!!Check if he hasn't taken any loan before
-        addressToLoanAmount[msg.sender] == 0;
-
-        // !!!!!!!Get LP Token which will be 60% of total amount
+        addressToLoanAmount[msg.sender] = 0;
         BorrowedLoanInfo[msg.sender].loanApprovedOfInDollar = 0;
 
-        // !!!!!!!Loan Details
         BorrowedLoanInfo[msg.sender] = LoanInfo(
             payable(msg.sender),
             block.timestamp,
@@ -168,10 +179,132 @@ contract LendingPlatformInheritance is LPToken {
             0,
             true
         );
+    }
 
-        // !!!!add his address to list
+    // // // // // // // // // // // // // //
+    // Functionalities for Lender
 
-        // !!!!!!!Assigning Loan Amount
-        addressToLoanAmount[msg.sender] = 0;
+    // Firstly a lender will invest crypto and will not get anything back
+    // The Crypto Investment will be converted to LPs and thos LPs will be added
+    // to LendingPlatform Smart Contract so that whenevery a borrower need to borrow LPs,
+    // Platform will not generate new LP.
+
+    function invest() public payable {
+        // First he has to Pay Complete Amount.
+        // BUT DUE TO MINT FUNCTIONALY
+        // Therefore Let Lender mint first and then transfer Minted tokens to LP.
+        mint(msg.value);
+        _transfer(msg.sender, address(this), EthToLP(msg.value));
+
+        // Now Make him Pay the Crypto Investment Amount
+        bool success = payable(address(this)).send(msg.value);
+        require(success, "Call Failed to Invest");
+
+        // IMPORTANT Now LP has Crypto + Minted Token
+        // // // // // // // // // //
+
+        // // // // // // // // // //
+        //  Fill All required information about invester's investment
+
+        totalInvester += 1;
+        investers.push(msg.sender);
+        addressToInvestmentAmountInWei[msg.sender] = msg.value;
+
+        InvestmentInfo[msg.sender] = InvesterInfo(
+            msg.sender,
+            msg.value,
+            block.timestamp,
+            EthToLP(msg.value),
+            calculateInterestToBeReturnedToInvester(),
+            false
+        );
+    }
+
+    function calculateInterestToBeReturnedToInvester()
+        public
+        returns (uint256)
+    {
+        // Time Variables
+        uint256 time1;
+        uint256 time2;
+        uint256 timeDifference;
+
+        time1 = InvestmentInfo[msg.sender].startInvestmentTime;
+        time2 = block.timestamp;
+        timeDifference = time2 - time1;
+
+        // Simple Interest
+        // 14% return interest on Investment for an Year
+        // Therefore (r(in %) * t(in min) = 14*1/100*365*24*60 = 0.000000266362253
+
+        // Interest based on minutes
+        // * Currently I'm Multiplying this equation with 1000 to check interest amount
+        // * That means 1 min = 1000 min
+        interestToBeReturnedInWei[msg.sender] =
+            (1000 *
+                ((timeDifference / 60) *
+                    InvestmentInfo[msg.sender].investmentAmountInWei *
+                    14)) /
+            (100 * 365 * 24 * 60);
+        InvestmentInfo[msg.sender]
+            .ApproxInterestGained = interestToBeReturnedInWei[msg.sender];
+
+        return interestToBeReturnedInWei[msg.sender];
+    }
+
+    function getCryptoBackWithInterest() public payable {
+        // Check Did msg.sender has invested and Was the Investment returned with Interest
+        require(
+            InvestmentInfo[msg.sender].investmentAmountInWei > 0 &&
+                InvestmentInfo[msg.sender].didLPReturnedWithProfit == false
+        );
+
+        // Now in Order to Return
+        // 1. Does LP currently have sufficient amount of LPToken (which was genereted) to 'burn again'
+        // 'Burn Again' Here means to Exchange them in crypto and send
+        // 2. Does LP Smart Contract currently have sufficient Crypto to return as interest?
+        require(
+            balanceOf(address(this)) >=
+                InvestmentInfo[msg.sender].amountOfLPGenerated
+        );
+        require(
+            address(this).balance >= calculateInterestToBeReturnedToInvester()
+        );
+
+        // // // // // // // // // // // //
+        // Return The Crypto back to Invester with Interest
+
+        // 1. Need to transfer LPToken to msg.sender in order to get him refund
+        _transfer(
+            address(this),
+            msg.sender,
+            InvestmentInfo[msg.sender].amountOfLPGenerated
+        );
+        // 2. Initiate Refund to Burn LPToken and Transfer crypto from SmartContract to Invester
+        refund(InvestmentInfo[msg.sender].amountOfLPGenerated);
+        // 3. Return the Amount of Interest in Crypto
+        bool success = payable(msg.sender).send(
+            calculateInterestToBeReturnedToInvester()
+        );
+        require(success, "Call Failed");
+
+        // // // // // // // // // // // //
+
+        // // // // // // // // // // // //
+        // CHANGE EVERYTHING BACK TO AS IT WAS
+
+        totalInvester -= 1;
+        addressToInvestmentAmountInWei[msg.sender] = 0;
+
+        InvestmentInfo[msg.sender] = InvesterInfo(
+            msg.sender,
+            0,
+            0,
+            EthToLP(0),
+            0,
+            true
+        );
+
+        // // // // // // // // // // // //
     }
 }
